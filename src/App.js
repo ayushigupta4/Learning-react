e793cf3294ee33a9cf1a9e0a26116d89561435b1
Ayushi <ayushi.gupta.038@gmail.com>
2024-03-17 05:23:20 +0530

    Add dark/light toggle feature

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#454545';
+    }  
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+      
+  }
   return (
     <>
 
-      <Navbar title="TextUtils" home="Home" about="About Us"/>
+      <Navbar title="TextUtils" home="Home" about="About Us" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
         
-        <TextForm heading="Enter text to analyse"/>
+        <TextForm heading="Enter text to analyse" mode={mode}/>
 
         <About/>
       </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,9 +20,22 @@ export default function TextForm(props) {
     }
 
     const textToSpeech = () => {
-        let speech = new SpeechSynthesisUtterance();
-        speech.text = text;
-        window.speechSynthesis.speak(speech);
+        //Split text into chunks
+        const chunkSize = 200;
+        const chunks = text.match(new RegExp(`.{1,${chunkSize}}`, 'g')) || [];
+
+        //speak each chunk
+        chunks.forEach(chunk => {
+            let speech = new SpeechSynthesisUtterance();
+            speech.text = chunk;
+            window.speechSynthesis.speak(speech);
+        });
+        
+    }
+
+    const removeExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
     }
 
     const handleOnChange = (event) => {
@@ -34,22 +47,23 @@ export default function TextForm(props) {
     //setText("Write text here");
     return (
         <>
-            <div className='container'>
+            <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to uppercase</button>
                 <button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to lowercase</button>
                 <button className="btn btn-success mx-2" onClick={clearText}>Clear Text</button>
                 <button className="btn btn-success mx-2" onClick={textToSpeech}>Listen</button>
+                <button className="btn btn-success mx-2" onClick={removeExtraSpaces}>Remove extra spaces</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter something in textbox to preview"}</p>
             </div>
         </>
   )
